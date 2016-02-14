159d5b4cf0e383a73fbdfcd5e7283d445b02516e
Oskar Niburski <oskarniburski@gmail.com>
2016-02-14 18:49:19 -0500

    Boilerplattteee!

---
+++ Music.js
@@ -0,0 +1,53 @@
+var React = require('react-native');
+
+var {
+  Platform,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  View,
+  Text,
+  TouchableElement,
+  StyleSheet,
+  Dimensions,
+  Image
+} = React;
+
+let screenHeight = Dimensions.get('window').height;
+
+class Music extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  goBack() {
+    this.props.navigator.pop();
+  }
+
+  render() {
+    return (
+      <View style={styles.thing}>
+        <Text>Music</Text>
+        <TouchableHighlight
+          onPress={this.goBack.bind(this)}
+        >
+          <Text style={styles.buttonText}>Go Back!</Text>
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  thing: {
+    alignSelf: 'center',
+    padding: 20,
+    paddingTop: 50,
+    width: screenHeight * 0.5,
+    height: screenHeight * 0.1,
+    borderColor: 'gray', 
+    borderWidth: 1
+  },
+});
+
+module.exports = Music;
+
